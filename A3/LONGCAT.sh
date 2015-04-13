2039a2911e0c72121bfa5dcd23838ac51bc95e1b
long <longsha@172-17-77-178.DYNAPOOL.NYU.EDU>
2015-04-13 18:49:44 -0400

    final submission for LONGCAT.sh

--- A3/LONGCAT.sh
+++ A3/LONGCAT.sh
@@ -12,5 +12,14 @@ cp /scratch/ls3470/DeepLearning/longcat/A3/baseline/A3_baseline.lua ./
 cp /scratch/ls3470/DeepLearning/longcat/A3/LONGCAT_model.sh ./
 cp /scratch/ls3470/DeepLearning/longcat/A3/run_model_on_stdin.lua ./
 
+mkdir ./newmodel
+cp /scratch/ls3470/DeepLearning/longcat/A3/newmodel/doall.lua ./newmodel
+cp /scratch/ls3470/DeepLearning/longcat/A3/newmodel/load_glove.lua ./newmodel
+cp /scratch/ls3470/DeepLearning/longcat/A3/newmodel/model_morefanout_bigger.lua ./newmodel
+cp /scratch/ls3470/DeepLearning/longcat/A3/newmodel/preprocess_glove_plain_100.lua ./newmodel
+cp /scratch/ls3470/DeepLearning/longcat/A3/newmodel/opt.lua ./newmodel
+cp /scratch/ls3470/DeepLearning/longcat/A3/newmodel/test_model.lua ./newmodel
+cp /scratch/ls3470/DeepLearning/longcat/A3/newmodel/train_model.lua ./newmodel
+
 cp /scratch/ls3470/DeepLearning/longcat/A3/LONGCAT.pdf ./
 cp /scratch/ls3470/DeepLearning/longcat/A3/model.net ./
